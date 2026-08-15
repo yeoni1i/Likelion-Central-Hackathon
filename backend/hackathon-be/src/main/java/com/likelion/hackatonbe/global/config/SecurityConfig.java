d52e7008fc710c3247a5a2a252f0f6367c59b4f7
eunsol <cindychoi03@naver.com>
2026-08-16 02:58:55 +0900

    feat: 날씨 조회 1시간 쿨타임 적용 및 30일 이력 삭제 스케줄러 구현

--- backend/hackathon-be/src/main/java/com/likelion/hackatonbe/global/config/SecurityConfig.java
+++ backend/hackathon-be/src/main/java/com/likelion/hackatonbe/global/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         // 로그인, 회원가입 1단계, 완료 페이지는 인증 없이 허용
-                        .requestMatchers("/accounts/", "/accounts/signup_account", "/accounts/signup_end","/api/weather/**").permitAll()
+                        .requestMatchers("/accounts/", "/accounts/signup_account", "/accounts/signup_end","/error").permitAll()
                         // 그 외 모든 요청(signup_child 포함)은 JWT 인증 필요
                         .anyRequest().authenticated()
                 )
